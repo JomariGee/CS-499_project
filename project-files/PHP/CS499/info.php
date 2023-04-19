1e394e189e9751be4fbf2dbff256650068ab5495
bensonhaley01 <80924125+bensonhaley01@users.noreply.github.com>
2023-04-19 18:50:50 -0500

    Update info.php

--- project-files/PHP/CS499/info.php
+++ project-files/PHP/CS499/info.php
@@ -1,22 +1,66 @@
 <?php
-// NEED TO EXECUTE database_backup.sql WITH SCHEMA "cpg" ! 
+// NEED TO UPDATE SO THAT DROPDOWN DOESNT SET ALL STUFF UNCHANGED TO 0!
 
 // Connect to the database
 require_once('mysqli_connect.php');
+
 $id = $_GET['goalID'];
 if (!$id){
-	echo "<script language='javascript'>window.alert('Your Message');window.location='http://localhost/CS499/goals.php';</script>";
+	echo "<script language='javascript'>window.alert('Error: returning to main page');window.location='goals.php';</script>";
 }
-$sql = "select * from status_update su 
-	right join 
-	(select g.goalID as GoalID, g.goalTitle as Title, c.category_desc as Cat, 
-	g.status_updateID as StatUpdate, g.cost, g.impact, g.complexity 
-	from goal g 
-	left join category c 
-	on g.categoryID=c.categoryID ) 
-	AS gc 
-	on su.stat_updateID=gc.StatUpdate 
-	where gc.goalID=$id";
+
+// Get current ID from URL parameter
+$current_id = $_GET['goalID'];
+
+// Retrieve all goal IDs from the database
+$prev_next_sql = "SELECT goalID FROM goal";
+$prev_next_response = mysqli_query($dbc, $prev_next_sql);
+if (!$prev_next_response) {
+    die('Error retrieving goal IDs: ' . mysqli_error($dbc));
+}
+
+$id_array = array();
+
+while ($row = mysqli_fetch_assoc($prev_next_response)) {
+    $id_array[] = $row['goalID'];
+}
+
+// Find the index of the current ID in the array
+$current_index = array_search($current_id, $id_array);
+
+// Get the previous ID
+$previous_index = $current_index - 1;
+if ($previous_index < 0) {
+    $previous_id = $id_array[count($id_array) - 1]; // Wrap around to last ID if at beginning of array
+} else {
+    $previous_id = $id_array[$previous_index];
+}
+
+// Get the next ID
+$next_index = $current_index + 1;
+if ($next_index >= count($id_array)) {
+    $next_id = $id_array[0]; // Wrap around to first ID if at end of array
+} else {
+    $next_id = $id_array[$next_index];
+}
+
+// Output links to previous and next pages
+echo "<a href='info.php?goalID=$previous_id'>Previous</a>";
+echo "<a href='info.php?goalID=$next_id'>Next</a>";
+
+
+
+$sql = "SELECT * FROM status_update su 
+		RIGHT JOIN (
+			SELECT g.goalID AS GoalID, g.goalTitle AS Title, c.category_desc AS Cat, 
+			g.status_updateID, g.cost, g.impact, g.complexity, s.status_desc AS StatUpdate
+			FROM goal g 
+			LEFT JOIN category c ON g.categoryID = c.categoryID 
+			LEFT JOIN status s ON g.status_updateID = s.statusID
+		) AS gc ON su.stat_updateID = gc.StatUpdate 
+		WHERE gc.goalID = $id";
+
+
 $response = mysqli_query($dbc, $sql);
 
 if ($response) {
@@ -26,45 +70,42 @@ if ($response) {
 	$cost = $row['cost'];
 	$impact = $row['impact'];
 	$complexity = $row['complexity'];
-	$status = $row['statusID'];
+	$status = $row['StatUpdate'];
 	
-	if ($cost == 0)
-		$cost = "$";
-	elseif ($cost == 1)
-		$cost = "$$";
-	elseif ($cost == 2)
-		$cost = "$$$";
-		
-	if ($impact == 0)
-		$impact = "Low";
-	elseif ($impact == 1)
-		$impact = "Medium";
-	elseif ($impact == 2)
-		$impact = "High";
-		
-	if ($complexity == 0)
-		$complexity = "Low";
-	elseif ($complexity == 1)
-		$complexity = "Medium";
-	elseif ($complexity == 2)
-		$complexity = "High";
 
-	if ($status == 1)
-		$status = "Not Started";
-	elseif ($status == 2)
-		$status = "Scoped";
-	elseif ($status == 3)
-		$status = "In Progress";
-	elseif ($status == 4)
-		$status = "Implemented";
+	if ($cost == 1)
+		$cost = "$";
+	elseif ($cost == 2)
+		$cost = "$$";
+	elseif ($cost == 3)
+		$cost = "$$$";
+	elseif ($cost == 4)
+		$cost = "$$$$";
+	
+		
+		
+	if ($impact == 1)
+		$impact = "Low";
+	elseif ($impact == 2)
+		$impact = "Medium";
+	elseif ($impact == 3)
+		$impact = "High";
+	
+		
+	if ($complexity == 1)
+		$complexity = "Low";
+	elseif ($complexity == 2)
+		$complexity = "Medium";
+	elseif ($complexity == 3)
+		$complexity = "High";
+	
+	
 	
 	$sql = "select * from risk r join goal_risk gr on r.riskID=gr.riskID where gr.goalID=$id;";
 	$response = @mysqli_query($dbc, $sql);
-    $risk="";
-	$str='<br>';
+    $risk="";       
     if($response){
         while($row = mysqli_fetch_array($response)){
-			$risk.=$str;
 			$risk.=$row['risk_desc'];
 		}
 	}
@@ -107,17 +148,44 @@ mysqli_close($dbc);
                 <li><a href="goals.php">Goals</a></li>
                 <li><a href="info.php">Info</a></li>
                 <li><a href="recommended-action.html">Recommended Action</a></li>
+                <li><a href="assessments.html">Assessment History</a></li>
+            </ul>
+        </div>
+		
+		
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Info</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" href="main.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    </head>
+    <body>
+        <!-- Hamburger Menu & Title -->
+
+
+        <!-- Navigation bar -->
+        <div class="nav">
+            <ul>
+                <li><a href="goals.php">Goals</a></li>
+                <li><a href="info.php">Info</a></li>
+                <li><a href="recommended-action.html">Recommended Action</a></li>
                 <li><a href="assessments.php">Assessment History</a></li>
             </ul>
         </div>
 		
         <!-- Goal -->
 		<div class="main">
+
+
+
             <div class="Rectangle45">
                 <p><b>Goal:</b> <?php echo $title; ?></p>
             </div>
-			
-		<!-- Category -->
+
+        <!-- Category -->
             <div class="Rectangle46">
                 <p><b>Category:</b> <?php echo $cat; ?></p>
             </div>
@@ -144,9 +212,10 @@ mysqli_close($dbc);
 		
 		<!-- Risks Addressed -->
             <div class="Rectangle51">
-                <p><b>Risks Addressed:</b><?php echo $risk; ?></p>
+                <p><b>Risks Addressed:</b><br><?php echo $risk; ?></p>
             </div>
 		
+
 		<!-- Hamburger Menu animation -->
         <script>
             $(document).ready(function() {
