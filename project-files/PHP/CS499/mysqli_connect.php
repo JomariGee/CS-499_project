39525de52839111a45d9cf55dc61d6fa23634344
bensonhaley01 <80924125+bensonhaley01@users.noreply.github.com>
2023-04-19 18:56:43 -0500

    Update mysqli_connect.php

--- project-files/PHP/CS499/mysqli_connect.php
+++ project-files/PHP/CS499/mysqli_connect.php
@@ -7,7 +7,7 @@
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_USER', 'root');
 DEFINE ('DB_PASSWORD', '');
-DEFINE ('DB_NAME', 'cs499');
+DEFINE ('DB_NAME', 'cpg');
 // $dbc will contain a resource link to the database
 // @ keeps the error from showing in the browser
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
