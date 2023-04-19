94c174c6cf48d13d89e77ff0fe4c74fe5dc77990
lindsey0215 <91569170+lindsey0215@users.noreply.github.com>
2023-04-18 20:29:19 -0500

    Update goals.php

--- project-files/PHP/CS499/goals.php
+++ project-files/PHP/CS499/goals.php
@@ -283,7 +283,7 @@
         <div class="nav">
             <ul>
                 <li><a href="goals.php">Goals</a></li>
-                <li><a href="info.html">Info</a></li>
+                <li><a href="info.php">Info</a></li>
                 <li><a href="recommended-action.html">Recommended Action</a></li>
                 <li><a href="assessments.html">Assessment History</a></li>
             </ul>
