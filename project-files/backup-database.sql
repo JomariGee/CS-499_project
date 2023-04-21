3e1dd823fe499710970cdf9912668f0da1b29407
Impennington <isaacmpennington@gmail.com>
2023-04-21 16:56:51 -0500

    Revert "Merge branch 'main' of https://github.com/JomariGee/CS-499_project"
    This reverts commit 8dae6858608fee3c32b30d4490149dd2bd4d587f, reversing
    changes made to 716e60246e9436f0ca5213a121a46ac518947ef1.

--- project-files/backup-database.sql
+++ project-files/backup-database.sql
@@ -69,72 +69,51 @@ CREATE TABLE `goal` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 insert into goal (goalTitle, cost, impact, complexity, categoryID, csf, status_updateID) values 
-CREATE TABLE `goal` (
-  `goalID` int(11) NOT NULL AUTO_INCREMENT,
-  `goalTitle` varchar(255) DEFAULT NULL,
-  `status_updateID` int(11) DEFAULT NULL,
-  `cost` int(11) DEFAULT NULL,
-  `impact` int(11) DEFAULT NULL,
-  `complexity` int(11) DEFAULT NULL,
-  `categoryID` int(11) DEFAULT NULL,
-  `csf` varchar(255) DEFAULT NULL,
-  `assessment_date` varchar(50) DEFAULT NULL, 
-  PRIMARY KEY (goalID)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-insert into goal (goalTitle, cost, impact, complexity, categoryID, csf, status_updateID) values 
-/*ACCOUNT SECURITY (1.0) -- checked*/
-	("Detection of Unsuccessful (Automated) Login Attempts", 1, 3, 1, 1, "PR.AC-7", 1), 
-	("Changing Default Passwords", 1, 3, 2, 1, "PR.AC-1", 1),
-	("Multi-Factor Authentication (MFA)", 2, 3, 2, 1, "PR.AC-7", 1), 
-	("Minimum Password Strength", 1, 3, 1, 1, "PR.AC-1", 1),
-	("Separating User and Privileged Accounts", 1, 3, 1, 1, "PR.AC-4", 1),
-	("Unique Credentials", 2, 2, 2, 1, "PR.AC-1", 1),
-	("Revoking Credentials for Departing Employees", 1, 2, 1, 1, "PR.AC-1", 1), 
-	
-/*DEVICE SECURITY (2.0) -- checked*/ 
-	("Hardware and Software Approval Process", 2, 3, 2, 2, "PR.IP-3", 1), 
-	("Disable Macros by Default", 1, 2, 1, 2, "PR.IP-1, PR.IP-3", 1),
-	("Asset Inventory", 2, 3, 2, 2, "ID.AM-1", 1), 
-	("Prohibit Connection of Unauthorized Devices", 3, 3, 3, 2, "PR.PT-2", 1), 
-	("Document Device Configurations", 2, 3, 2, 2, "PR.IP-1", 1), 
-	
-/*DATA SECURITY (3.0) -- checked*/
-	("Log Collection", 2, 3, 2, 3, "PR.PT-1", 1), 
-	("Secure Log Storage", 3, 3, 1, 3, "PR.PT-1", 1),
-	("Strong and Agile Encryption", 2, 3, 2, 3, "PR.DS-1, PR.DS-2", 1), 
-	("Secure Sensitive Data", 2, 3, 2, 3, "PR.DS-1, PR.DS-2, PR.DS-5", 1), 
-	
-/*GOVERNANCE AND TRAINING (4.0) -- checked*/
-	("Organizational Cybersecurity Leadership", 1, 3, 1, 4, "ID.GV-1, ID.GV-2", 1), 
-	("OT Cybersecurity Leadership", 1, 3, 1, 4, "ID.GV-1, ID.GV-2", 1), 
-	("Basic Cybersecurity Training", 1, 3, 1, 4, "PR.AT-1", 1), 
-	("OT Cybersecurity Training", 1, 3, 1, 4, "PR.AT-2, PR.AT-3, PR.AT-5", 1), 
-	("Improving IT and OT Cybersecurity Relationships", 1, 2, 1, 4, "ID.GV-2", 1), 
-	
-/*VULNERABILITY MANAGEMENT (5.0) -- checked*/
-	("Mitigating Known Vulnerabilities", 1, 3, 2, 5, "PR.IP-12, ID.RA-1, DE.CM-8, RS.MI-3", 1), 
-	("Vulnerability Disclosure/Reporting", 3, 1, 3, 5, "RS.AN-5", 1), 
-	("Deploy Security.txt Files", 1, 3, 1, 5, "RS.AN-5", 1), 
-	("No Exploitable Services on the Internet", 1, 3, 1, 5, "PR.PT-4", 1), 
-	("Limit OT Connections to Public Internet", 3, 2, 2, 5, "PR.PT-4", 1), 
-	("Third-Party Validation of Cybersecurity Control Effectiveness", 3, 3, 3, 5, "ID.RA-1, ID.RA-3", 1), 
-	
-/*SUPPLY CHAIN / THIRD PARTY (6.0) -- checked*/
-	("Vendor/Supplier Cybersecurity Requirements", 1, 3, 1, 6, "ID.SC-3", 1), 
-	("Supply Chain Incident Reporting", 1, 3, 1, 6, "ID.SC-1, ID.SC-3", 1), 
-	("Supply Chain Vulnerability Disclosure", 1, 3, 1, 6, "ID.SC-1, ID.SC-3", 1), 
-	
-/* Response and Recovery -- checked*/
-	(" Incident Reporting", 1, 3, 1, 7, "RS.CO-2, RS.CO-4", 1), 
-	("Incident Response (IR) Plans", 1, 3, 1, 7, "PR.IP-9, PR.IP-10", 1), 
-	("System Back Ups", 2, 3, 2, 7, "PR.IP-4", 1), 
-	("Document Network Topology", 2, 2, 2, 7, "PR.IP-1", 1), 
-	
-/*Other (8.0) -- checked*/
-	("Network Segmentation", 3, 3, 3, 8, "PR.AC-5, PR.PT-4, DE.CM-1", 1), 
-	("Detecting Relevant Threats and TTPs", 3, 2, 3, 8, "ID.RA-3, DE.CM-1", 1), 
-	("Email Security", 1, 2, 1, 8, "PR.DS-1, PR.DS-2, PR.DS-5", 1);
+/*ACCOUNT SECURITY (1.0)*/
+	("Detection of Unsuccessful (Automated) Login Attempts", 1, 1, 0, 1, "PR.AC-7", 1), 
+	("Changing Default Passwords", 1, 1, 1, 1, "PR.AC-1", 1),
+	("Multi-Factor Authentication (MFA)", 2, 2, 1, 1, "PR.AC-7", 1), 
+	("Minimum Password Strength", 1, 2, 0, 1, "PR.AC-1", 1),
+	("Separating User and Privileged Accounts", 1, 2, 0, 1, "PR.AC-4", 1),
+	("Unique Credentials", 2, 1, 1, 1, "PR.AC-1", 1),
+	("Revoking Credentials for Departing Employees", 1, 1, 0, 1, "PR.AC-1", 1), 
+/*DEVICE SECURITY (2.0)*/
+	("Hardware and Software Approval Process", 2, 2, 1, 2, "PR.IP-3", 1), 
+	("Disable Macros by Default", 1, 1, 0, 2, "PR.IP-1, PR.IP-3", 1),
+	("Asset Inventory", 2, 2, 1, 2, "ID.AM-1", 1), 
+	("Prohibit Connection of Unauthorized Devices", 3, 2, 2, 2, "PR.PT-2", 1), 
+	("Document Device Configurations", 2, 2, 1, 2, "PR.IP-1", 1), 
+/*DATA SECURITY (3.0)*/
+	("Log Collection", 2, 2, 1, 3, "PR.PT-1", 1), 
+	("Secure Log Storage", 3, 2, 0, 3, "PR.PT-1", 1),
+	("Strong and Agile Encryption", 2, 2, 1, 3, "PR.DS-1, PR.DS-2", 1), 
+	("Secure Sensitive Data", 2, 2, 1, 3, "PR.DS-1, PR.DS-2, PR.DS-5", 1), 
+/*GOVERNANCE AND TRAINING (4.0)*/
+	("Organizational Cybersecurity Leadership", 1, 2, 0, 4, "ID.GV-1, ID.GV-2", 1), 
+	("OT Cybersecurity Leadership", 1, 2, 0, 4, "ID.GV-1, ID.GV-2", 1), 
+	("Basic Cybersecurity Training", 1, 2, 0, 4, "PR.AT-1", 1), 
+	("OT Cybersecurity Training", 1, 2, 0, 4, "PR.AT-2, PR.AT-3, PR.AT-5", 1), 
+	("Improving IT and OT Cybersecurity Relationships", 1, 1, 0, 4, "ID.GV-2", 1), 
+/*VULNERABILITY MANAGEMENT (5.0)*/
+	("Mitigating Known Vulnerabilities", 1, 2, 1, 5, "PR.IP-12, ID.RA-1, DE.CM-8, RS.MI-3", 1), 
+	("Vulnerability Disclosure/Reporting", 3, 0, 2, 5, "RS.AN-5", 1), 
+	("Deploy Security.txt Files", 1, 2, 0, 5, "RS.AN-5", 1), 
+	("No Exploitable Services on the Internet", 1, 2, 0, 5, "PR.PT-4", 1), 
+	("Limit OT Connections to Public Internet", 3, 1, 1, 5, "PR.PT-4", 1), 
+	("Third-Party Validation of Cybersecurity Control Effectiveness", 3, 2, 2, 5, "ID.RA-1, ID.RA-3", 1), 
+/*SUPPLY CHAIN / THIRD PARTY (6.0)*/
+	("Vendor/Supplier Cybersecurity Requirements", 1, 2, 0, 6, "ID.SC-3", 1), 
+	("Supply Chain Incident Reporting", 1, 2, 0, 6, "ID.SC-1, ID.SC-3", 1), 
+	("Supply Chain Vulnerability Disclosure", 1, 2, 0, 6, "ID.SC-1, ID.SC-3", 1), 
+/* Response and Recovery */
+	(" Incident Reporting", 1, 2, 0, 7, "RS.CO-2, RS.CO-4", 1), 
+	("Incident Response (IR) Plans", 1, 2, 0, 7, "PR.IP-9, PR.IP-10", 1), 
+	("System Back Ups", 2, 2, 1, 7, "PR.IP-4", 1), 
+	("Document Network Topology", 2, 1, 1, 7, "PR.IP-1", 1), 
+/*Other (8.0)*/
+	("Network Segmentation", 3, 2, 2, 8, "PR.AC-5, PR.PT-4, DE.CM-1", 1), 
+	("Detecting Relevant Threats and TTPs", 3, 1, 2, 8, "ID.RA-3, DE.CM-1", 1), 
+	("Email Security", 1, 1, 0, 8, "PR.DS-1, PR.DS-2, PR.DS-5", 1);
 
 -- --------------------------------------------------------
 
