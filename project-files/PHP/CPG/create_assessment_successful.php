800b15105e3893b99163390bf07b9264d51e4151
lindsey0215 <91569170+lindsey0215@users.noreply.github.com>
2023-04-22 00:58:54 -0500

    Update create_assessment_successful.php

--- project-files/PHP/CPG/create_assessment_successful.php
+++ project-files/PHP/CPG/create_assessment_successful.php
@@ -7,7 +7,7 @@
     $goalNum;
     $status;
     $assessment_date;  
-	$note_desc = NULL;
+	$note;
 
     if(isset($_POST['submit'])){
         // create array
@@ -24,17 +24,110 @@
 
         // with the rest of the options, they are able to be left unchanged
 
-0		//Status
-    0    if(isset($_POST['status']) && !empty($_POST['status'])){
- 9           $status = $_POST['status'];
- 0       }else{
-			// NEED TO UPDATE SO THAT DROPDOWN DOESNT SET ALL STUFF UNCHANGED TO 0!
-
+		//Status
+        if(isset($_POST['status']) && !empty($_POST['status'])){
+            $status = $_POST['status'];
+			$newest_status = 1;
+			
+        }else{				
 			// Connect to the database
 			require_once('mysqli_connect.php');
 
 			// Retrieve all goals and ids
-			$sql = "SELECT g.goalID, g.goalTitle 
+			$sql = "SELECT g.goalID, su.stat_updateID, su.statusID 
+						FROM goal g 
+						LEFT JOIN status_update su 
+						on g.status_updateID=su.stat_updateID where g.goalID=$goalNum;";
+
+			$response = mysqli_query($dbc, $sql);
+			if($response){
+				$status = $row['statusID'];
+			}
+			$newest_status = 0;
+        }
+		
+		//Assessment Date
+		if(isset($_POST['assessment_date']) && !empty($_POST['assessment_date'])){
+            $assessment_date = $_POST['assessment_date'];
+        }else{
+            // instead of null, set the date to today
+            $assessment_date = date("Y-m-d");
+        }
+
+        if(isset($_POST['note_desc']) && !empty($_POST['note_desc'])){
+            $note = ($_POST['note_desc']);
+			echo $note;
+		}
+
+        if(empty($data_missing)){
+			require_once('mysqli_connect.php');
+            $query = "INSERT INTO status_update (goalID, statusID, update_date) 
+						values ($goalNum, $status, '$assessment_date'); ";
+			if(!empty($_POST['note_desc'])){
+				$sql="INSERT INTO notes (note_desc, goalID) 
+						values ('$note', $goalNum);";
+				mysqli_query($dbc, $sql);
+			}
+
+			
+			if (mysqli_query($dbc, $query)) {
+				echo "<script language='javascript'>window.alert('New record created successfully');window.location='create_assessment.php';</script>";
+			}else {
+				echo "Error: " . $query . "<br>" . $conn->error;
+			}
+			mysqli_close($dbc);
+        } else {
+            echo '<br /><br /><b>You need to enter the Title!</b><br /><br />';
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Assessments </title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" href="main.css" type="text/css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    </head>
+    <body>
+        <!-- Hamburger Menu & Title -->
+        <div class="header">
+            <div class="hamburger-menu">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <h1 class="title">Create New Assessment</h1>
+        </div>
+
+        <!-- Navigation bar --
+        <div class="nav">
+            <ul>
+                <li><a href="goals.php">Goals</a></li>
+                <li><a href="assessment_history.php">Assessment History</a></li>
+            </ul>
+        </div>
+        <br><br><br> -->
+
+
+   <form action="create_assessment_successful.php" method="post">
+
+			<!--Choose Goal-->
+			<p>Goal: <br>
+                <select id="goalNum" name="goalNum">   
+					<option value="0">--Select Goal--</option>
+				<!-- LOOP FOR EACH GOAL -->
+					<?php
+						// NEED TO UPDATE SO THAT DROPDOWN DOESNT SET ALL STUFF UNCHANGED TO 0!
+
+						// Connect to the database
+						require_once('mysqli_connect.php');
+
+						// Retrieve all goals and ids
+						$sql = "SELECT g.goalID, g.goalTitle 
 							FROM goal g;";
 
 
@@ -52,144 +145,69 @@
 						}
 						// Close connection to the database
 						//mysqli_close($dbc);
-	 
-	 
-	 
- 0           // instead of null, set the status to same as before assessment
- 0           $status = 1;
-        }
-		
-		//Assessment Date
-		if(isset($_POST['assessment_date']) && !empty($_POST['assessment_date'])){
- 9           $assessment_date = $_POST['assessment_date'];
- 0       }else{
- 0           // instead of null, set the date to today
- 0           $assessment_date = date("m/d/y");
-        }
-
-        if(isset($_POST['note_desc']) && !empty($_POST['note_desc'])){
-            $note = $_POST['note_desc'];
-        }else{
-            $note = null;
-        }
-
-
-        if(empty($data_missing)){
-            require_once('mysqli_connect.php');
-            $query = "INSERT INTO goal (goalTitle, categoryID, status_updateID, cost, complexity, impact, assessment_date) VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-            $stmt = mysqli_prepare($dbc, $query);
-
-            mysqli_stmt_bind_param($stmt, 'sssssss', $goalTitle, $category, $status, $cost, $complexity, $impact, $today);
-            mysqli_stmt_execute($stmt);
-            $affected_rows = mysqli_stmt_affected_rows($stmt);
-            if($affected_rows == 1){
-                header('Location:goals.php');
-                exit;
-                mysqli_stmt_close($stmt);
-                mysqli_close($dbc);
-                } else {
-                echo 'Error Occurred<br />';
-                echo mysqli_error($dbc);
-                mysqli_stmt_close($stmt);
-                mysqli_close($dbc);
-            }
-        } else {
-
-            echo '<br /><br /><b>You need to enter the Title!</b><br /><br />';
-
-        }
-    }
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Goals</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-        <link rel="stylesheet" href="main.css" type="text/css">
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    </head>
-    <body>
-
-        <!-- Hamburger Menu & Title -->
-        <div class="header">
-            <h1 class="title">Create New Goal</h1>
-        </div>
-
-        <!-- Form --> 
-        <form action="create_goal_successful.php" method="post">
-            <!-- Title --> 
-            <p>Title: 
-                <br><input type="text" name="goalTitle" size="30" value="" />
-            </p>
-
-            <!-- Category --> 
-            <p>Category: 
-                <br>
-                <select id="category" name="category">                      
-                <option value="0">--Select Category--</option>
-                <option value="1">Account Security</option>
-                <option value="2">Device Security</option>
-                <option value="3">Data Security</option>
-                <option value="4">Governance and Training</option>
-                <option value="5">Vulnerability Management</option>
-                <option value="6">Supply Chain / Third Party</option>
-                <option value="7">Response and Recovery</option>
-                <option value="8">Other</option>
+					?>
                 </select>
             </p>
+			
 
-            <!-- Status --> 
-            <p>Status: 
-                <br>
+            <!-- Choose Status -->
+            <p>Status: <br>
                 <select id="status" name="status">                      
                 <option value="0">--Select Status--</option>
-                <option value="1">Not Started</option>
-                <option value="2">Scoped</option>
-                <option value="3">In Progress</option>
-                <option value="4">Implemented</option>
-                </select>
-            </p>
-            
-            <!-- Cost --> 
-            <p>Cost: 
-                <br>
-                <select id="cost" name="cost"> 
-                <option value="0">--Select Cost--</option>
-                <option value="1">$</option>
-                <option value="2">$$</option>
-                <option value="3">$$$</option>
-                <option value="4">$$$$</option>
-                </select>
-            </p>
-            
-            <!-- Complexity --> 
-            <p>Complexity: 
-                <br>
-                <select id="complexity" name="complexity"> 
-                <option value="0">--Select Complexity--</option>
-                <option value="1">Low</option>
-                <option value="2">Medium</option>
-                <option value="3">High</option>
+					<!-- LOOP FOR EACH Status -->
+					<?php
+						// NEED TO UPDATE SO THAT DROPDOWN DOESNT SET ALL STUFF UNCHANGED TO 0!
 
-                </select>
-            </p>
-            
-            <!-- Impact --> 
-            <p>Impact: 
-                <br>
-                <select id="impact" name="impact"> 
-                <option value="0">--Select Impact--</option>
-                <option value="1">Low</option>
-                <option value="2">Medium</option>
-                <option value="3">High</option>
+						// Connect to the database
+						require_once('mysqli_connect.php');
+
+						// Retrieve all goals and ids
+						$sql = "SELECT s.statusID, s.status_desc 
+							FROM status s;";
+
+
+						$response = mysqli_query($dbc, $sql);
+						if($response){
+							while($row = mysqli_fetch_assoc($response)){
+								$stat = $row['status_desc'];
+								$id = $row['statusID']; ?>
+						
+								<option value= "<?php echo $id; ?>"> <?php echo $stat;?> </option>
+					<?php	}
+						}else {
+							// Log the error to a file or send it to a logging service
+							error_log(mysqli_error($dbc));
+						}
+						// Close connection to the database
+						//mysqli_close($dbc);
+					?>
                 </select>
             </p>
 
-            <p> <input type="submit" name="submit" value="Create New Goal" /> </p>
+            <!--- Date Selection -->
+            <p>Date:
+            <input type="date" id="assessment_date" value="<?php echo date('Y-m-d'); ?>" />
+            <p>
+
+
+            <!-- Notes -->
+            <p>Notes:
+                <br><type="text" name="note_desc" size="30" value="" />
+            <p>
+
+            <p> <input type="submit" name="submit" value="Create New Assessment" /> </p>
         </form>
+     
+   <!-- PHP database code --> 
+   
+   <?php
+            // Get a connection for the database
+            require_once('mysqli_connect.php');
+
+            // Close connection to the database
+            mysqli_close($dbc);
+        ?> 
+
     </body>
 </html>
+
