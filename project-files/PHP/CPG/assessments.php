66faf10a14a6758a557c4004685761a72f64d098
lindsey0215 <91569170+lindsey0215@users.noreply.github.com>
2023-04-24 14:58:07 -0500

    Update assessments.php

--- project-files/PHP/CPG/assessments.php
+++ project-files/PHP/CPG/assessments.php
@@ -12,7 +12,7 @@ Need to update query so that it prints the assessments correctly
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Assessments For: </title>
+        <title> Assessments For Goal: </title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <link rel="stylesheet" href="main.css" type="text/css">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
