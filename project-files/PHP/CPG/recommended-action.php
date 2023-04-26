a7fa9c457b7eb82dd6a722aa10fe3e39026224c0
bensonhaley01 <80924125+bensonhaley01@users.noreply.github.com>
2023-04-25 21:34:51 -0500

    Update recommended-action.php

--- project-files/PHP/CPG/recommended-action.php
+++ project-files/PHP/CPG/recommended-action.php
@@ -31,8 +31,6 @@
 
         <!-- Database Code --> 
             <?php
-                // NEED TO UPDATE SO THAT DROPDOWN DOESNT SET ALL STUFF UNCHANGED TO 0!
-
                 // Connect to the database
                 require_once('mysqli_connect.php');
 
@@ -44,24 +42,36 @@
 
 
                 // PRINT THE INFORMATION
-                $sql = "SELECT recAction_desc, OT_desc, IT_desc FROM recommendedaction 
-                        WHERE goalID = $id";
+                $sql = "SELECT ra.recAction_desc, ra.OT_desc, ra.IT_desc, r.ref_Title, r.ref_link 
+FROM recommendedaction ra 
+LEFT JOIN referenceinstances ri ON ra.goalID = ri.goalID 
+LEFT JOIN ref r ON ri.referencesID = r.referencesID 
+WHERE ra.goalID = $id;
+";
+        
+
+
 
 
                 $response = mysqli_query($dbc, $sql);
 
                 if ($response) {
-                    $row = mysqli_fetch_assoc($response);
-                    if (!empty($row)) {
-                        $recAct = $row['recAction_desc'];
-                        $it_info = $row['IT_desc'];
-                        $ot_info = $row['OT_desc'];
-
-                    }
-                    else {
-                        $recAct = '';
-                    }
-                }
+    $row = mysqli_fetch_assoc($response);
+    if (!empty($row)) {
+        $recAct = $row['recAction_desc'];
+        $it_info = $row['IT_desc'];
+        $ot_info = $row['OT_desc'];
+        $ref_Title = $row['ref_Title'];
+        $ref_link = $row['ref_link'];
+    }
+    else {
+        $recAct = '';
+        $it_info = '';
+        $ot_info = '';
+        $ref_Title = '';
+        $ref_link = '';
+    }
+}
                 else {
                     // Log the error to a file or send it to a logging service
                     error_log(mysqli_error($dbc));
@@ -71,10 +81,35 @@
         <div class="main">
             
             <!-- Action recommendation  --> 
-            <div class= "Rectangle44-action-recommend">
+            <div class="">
                     <p><?php echo $recAct; ?></p>
                 </div>
             </div>
+
+                <!-- IT info  -->
+            <div class="">
+                    <p><?php echo $it_info; ?></p>
+                </div>
+            </div>
+
+                <!--  OT info  -->
+            <div class="">
+                    <p><?php echo $ot_info; ?></p>
+                </div>
+            </div>
+
+                <!-- Reference title  -->
+            <div class="">
+                    <p><?php echo $ref_Title; ?></p>
+                </div>
+            </div>
+
+		<!-- Reference link  -->
+            <div class="">
+                    <p><?php echo $ref_link; ?></p>
+                </div>
+            </div>
+
 		    <!-- Return button -->
                 <div class="return-to-info">
                     <a href="info.php?goalID=<?php echo $id; ?>"> Return to Info </a>
