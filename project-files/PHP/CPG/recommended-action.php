455b8c685c75eb85cd34ade4a7f8e4a8ce90e77b
lindsey0215 <91569170+lindsey0215@users.noreply.github.com>
2023-05-03 11:54:50 -0500

    Update recommended-action.php

--- project-files/PHP/CPG/recommended-action.php
+++ project-files/PHP/CPG/recommended-action.php
@@ -1,3 +1,50 @@
+       <!-- Database Code --> 
+            <?php
+                // Connect to the database
+                require_once('mysqli_connect.php');
+
+                // Get goalID from the url
+                $id = $_GET['goalID'];
+                if (!$id){
+                    echo "<script language='javascript'>window.alert('Error: returning to main page');window.location='goals.php';</script>";
+                }
+
+
+                // PRINT THE INFORMATION
+                $sql = "SELECT ra.recAction_desc, ra.OT_desc, ra.IT_desc, r.ref_Title, r.ref_link, g.goalTitle   
+			FROM recommendedaction ra 
+			LEFT JOIN referenceinstances ri ON ra.goalID = ri.goalID 
+			LEFT JOIN ref r ON ri.referencesID = r.referencesID
+            LEFT JOIN goal g ON g.goalID=ra.goalID 
+			WHERE ra.goalID = $id;
+			";
+        
+                $response = mysqli_query($dbc, $sql);
+
+                if ($response) {
+    $row = mysqli_fetch_assoc($response);
+    if (!empty($row)) {
+        $recAct = $row['recAction_desc'];
+        $it_info = $row['IT_desc'];
+        $ot_info = $row['OT_desc'];
+        $ref_Title = $row['ref_Title'];
+        $ref_link = $row['ref_link'];
+		$goalTitle = $row['goalTitle'];
+    }
+    else {
+        $recAct = '';
+        $it_info = '';
+        $ot_info = '';
+        $ref_Title = '';
+        $ref_link = '';
+    }
+}
+                else {
+                    // Log the error to a file or send it to a logging service
+                    error_log(mysqli_error($dbc));
+                }
+
+            ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -17,7 +64,7 @@
                     <div></div>
                     <div></div>
                 </div>
-                <h1 class="title">Recommended Action</h1>
+                <h1 class="title" style="font-size: 30px"><?php echo $goalTitle; ?></h1>
             </div>
 
         <!-- Navigation bar -->
@@ -28,78 +75,26 @@
                     <li><a href="create_assessment.php">Create New Assessment</a></li>
                 </ul>
             </div>
-
-        <!-- Database Code --> 
-            <?php
-                // Connect to the database
-                require_once('mysqli_connect.php');
-
-                // Get goalID from the url
-                $id = $_GET['goalID'];
-                if (!$id){
-                    echo "<script language='javascript'>window.alert('Error: returning to main page');window.location='goals.php';</script>";
-                }
-
-
-                // PRINT THE INFORMATION
-                $sql = "SELECT ra.recAction_desc, ra.OT_desc, ra.IT_desc, r.ref_Title, r.ref_link 
-			FROM recommendedaction ra 
-			LEFT JOIN referenceinstances ri ON ra.goalID = ri.goalID 
-			LEFT JOIN ref r ON ri.referencesID = r.referencesID 
-			WHERE ra.goalID = $id;
-			";
-        
-
-
-
-
-                $response = mysqli_query($dbc, $sql);
-
-                if ($response) {
-    $row = mysqli_fetch_assoc($response);
-    if (!empty($row)) {
-        $recAct = $row['recAction_desc'];
-        $it_info = $row['IT_desc'];
-        $ot_info = $row['OT_desc'];
-        $ref_Title = $row['ref_Title'];
-        $ref_link = $row['ref_link'];
-    }
-    else {
-        $recAct = '';
-        $it_info = '';
-        $ot_info = '';
-        $ref_Title = '';
-        $ref_link = '';
-    }
-}
-                else {
-                    // Log the error to a file or send it to a logging service
-                    error_log(mysqli_error($dbc));
-                }
-
-            ?>
+			
 		<!-- Return button -->
+
             <div class="return-to-info">
 				<a href="info.php?goalID=<?php echo $id; ?>"> Return to Info </a>
 			</div>
-
+				<br><h1 style="font-size: 30px"> Recommended Action </h1>
         <div class="main">
             <!--Recommended Action-->
 		<div class="Rectangle44-action-recommend">
 			<p class="recommended-action">
-			<b>Recommended Action:</b><br>
-			&nbsp; &nbsp;&nbsp;<?php echo $recAct; ?></p>
-		</div>
+			&nbsp; &nbsp;&nbsp;<?php echo $recAct; ?> <br><br><br><br>
+		<!--</p></div>
 					
                 <!-- IT info  -->
 		<?php 
-			if ($ot_info){
+			if ($it_info){
 				echo '
-					<div class="Rectangle-it">
-					<p class="it-action">
 						<b>IT:</b><br>
-						&nbsp; &nbsp;&nbsp; '.$it_info.'</p>
-					</div>
+						&nbsp; &nbsp;&nbsp; '.$it_info.'<br><br><br><br>
 				';
 			}
 
@@ -107,26 +102,21 @@
                 //  OT info
 			if ($ot_info){
 				echo '
-					<div class="Rectangle-ot">
-					<p class="ot-action">
 						<b>OT:</b><br>
-						&nbsp; &nbsp;&nbsp; '.$ot_info.'</p>
-					</div>
+						&nbsp; &nbsp;&nbsp; '.$ot_info.'<br><br><br><br>
 				';
 			}
-				
 				
 			if ($ref_Title){
 				echo '
-					<div class="Rectangle-resources">
-						<p class="resources">
+						<center>
 							<b>Resources:</b><br>
 							<a href="'.$ref_link.'">
-							'.$ref_Title.'</p>
-							</a>
-						</div>
+							'.$ref_Title.'
+							</a></center>
 				';
 			}
+			echo '</p></div>';
 		?>
 			
 		<!-- Hamburger Menu animation -->
